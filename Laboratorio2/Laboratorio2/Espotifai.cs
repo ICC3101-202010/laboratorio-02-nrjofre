bbf76788bb92d6cdda22b7907b38b3b537ac3fd1
Nicolás Jofré <nrjofre@miuandes.cl>
2020-03-30 02:55:24 -0300

    Incremeto 3, falta comprobar todas las funciones

--- Laboratorio2/Laboratorio2/Espotifai.cs
+++ Laboratorio2/Laboratorio2/Espotifai.cs
@@ -17,16 +17,89 @@ namespace Laboratorio2
             canciones = new Cancion[0];
             playlists = new Playlist[0];
         }
-        public bool ArgragarCancion(Cancion Cancion)
-        {
-            for each(Cancion cancionGuardada in Canciones)
+        public bool AgregarCancion(Cancion cancion)
+        {   // Comprueba si la cancion ya esta
+            foreach(Cancion cancionGuardada in canciones)
+            { if (cancionGuardada.nombre == cancion.nombre && cancionGuardada.artista == cancion.artista && cancionGuardada.album == cancion.album)
+                {
+                    return false;
+                }
+            }
 
+            Array.Resize(ref canciones, canciones.Length + 1);
+
+            canciones[canciones.Length - 1] = cancion;
             return true;
         }
 
         public void VerCanciones()
         {
-            
+            foreach(Cancion cancion in canciones)
+            {
+                Console.WriteLine(cancion.Informacion());
+            }
         }
-    }
+
+        public Cancion[] CancionesPorCriterio(string criterio, string valor)
+        {
+            Cancion[] filtrocancion = new Cancion[0];
+
+            if (criterio != "nombre" && criterio != "genero" && criterio != "album" && criterio != "artista")
+            {
+                Console.WriteLine("Criterio no valido. Ingrese nombre, genero, album o artista");
+                return filtrocancion;
+            }
+
+            foreach (Cancion cancion in canciones)
+            {
+                bool agregarCancion = false;
+
+                if (criterio == "nombre" && cancion.nombre == valor)
+                    agregarCancion = true;
+                else if (criterio == "genero" && cancion.genero == valor)
+                    agregarCancion = true;
+                else if (criterio == "album" && cancion.album == valor)
+                    agregarCancion = true;
+                else if (criterio == "artista" && cancion.artista == valor)
+                    agregarCancion = true;
+
+                if (agregarCancion)
+                {
+                    Array.Resize(ref filtrocancion, filtrocancion.Length + 1);
+                    filtrocancion[filtrocancion.Length - 1] = cancion;
+                }
+            }
+
+            return filtrocancion;
+        }
+
+        public bool GenerarPlaylist(string criterio, string valorCriterio, string nombrePlaylist)
+        {
+            foreach (Playlist playlist in playlists)
+                if (playlist.nombre == nombrePlaylist)
+                {
+                    Console.WriteLine("El nombre de esa Playlist ya existe");
+                    return false;
+                }
+            
+            Cancion[] filtrocancion = CancionesPorCriterio(criterio, valorCriterio);
+
+            if (filtrocancion.Length == 0)
+            {
+                Console.WriteLine("No hay canciones con ese criterio");
+                return false;
+            }
+
+            Array.Resize(ref playlists, playlists.Length + 1);
+            playlists[playlists.Length - 1] = new Playlist(nombrePlaylist, filtrocancion);
+            return true;
+        }
+
+        public void VerMisPlaylists()
+        {
+            foreach (Playlist playlist in playlists)
+                playlist.VerPlaylist(); 
+        }
+
+    }   
 }
--- Laboratorio2/Laboratorio2/Program.cs
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -8,11 +8,108 @@ namespace Laboratorio2
 {
     class Program
     {
-        static void Main(string[] args)
+        //cree un metodo para comprobar si el valor entregado por el usuario es un string
+        public static string PedirString(string explicacion)
         {
-            Cancion c = new Cancion("Lights", "Lights", "Ellie Goulding", "Pop");
-            c.Informacion();
-            Console.ReadLine();
+            string valor = "";
+            bool valido = false;
+            while (!valido)
+            {
+                Console.WriteLine(explicacion);
+                valor = Console.ReadLine();
+                if (valor.Trim() != "")
+                    valido = true;
+                else
+                    Console.WriteLine("Debes ingresar un valor valido");
+            }
+            return valor;
+        }
+
+        public static void ComandosDisponibles()
+        {
+            Console.WriteLine("\nLos comandos disponibles son:\n");
+            Console.WriteLine("1: Agregar cancion");
+            Console.WriteLine("2: Ver canciones");
+            Console.WriteLine("3: Ver canciones por criterio");
+            Console.WriteLine("4: Crear playlist");
+            Console.WriteLine("5: Ver mis playlists");
+            Console.WriteLine("0: Salir del programa\n");
+        }
+
+        public static void Main(string[] args)
+        {
+            // Partimos con 3 canciones en el programa por default
+            Espotifai espotifai = new Espotifai();
+            espotifai.AgregarCancion(new Cancion("Memories", "Memories", "Maroon 5", "Pop"));
+            espotifai.AgregarCancion(new Cancion("Lovely", "Lovely", "Billie Eilish", "Pop"));
+            espotifai.AgregarCancion(new Cancion("Tusa", "Tusa", "Karol G", "Urbano"));
+            
+            bool cerrarPrograma = false;
+            Console.WriteLine("Bienvenido a Espotifai");
+            
+            while (!cerrarPrograma)
+            {
+                ComandosDisponibles();
+                string comando = Console.ReadLine();
+                
+                if (comando == "1")
+                {
+                    string nombre = PedirString("Ingresa el nombre de la cancion");
+                    string album = PedirString("Ingresa el album de la cancion:");
+                    string artista = PedirString("Ingresa el artista de la cancion:");
+                    string genero = PedirString("Ingresa el género de la cancion:");
+                    
+                    Cancion cancion = new Cancion(nombre, album, artista, genero);
+                    
+                    if (espotifai.AgregarCancion(cancion))
+                    {
+                        Console.WriteLine("Se agrego la cancion.");
+                    }
+                }
+                
+                else if (comando == "2")
+                {
+                    espotifai.VerCanciones();
+                }
+                
+                else if (comando == "3")
+                {
+                    string criterio = PedirString("Ingresa un criterio (nombre, album, artista, genero)");
+                    string valor = PedirString("Ingresa un valor para el criterio");
+                    Cancion[] canciones = espotifai.CancionesPorCriterio(criterio, valor);
+                    
+                    if (canciones.Length == 0)
+                        Console.WriteLine("No se encontraron canciones");
+                    foreach (Cancion cancion in canciones)
+                        Console.WriteLine(cancion.Informacion());
+                }
+                
+                else if (comando == "4")
+                {
+                    string nombre = PedirString("Ingresa un nombre para la playlist:");
+                    string criterio = PedirString("Ingresa un criterio (nombre, album, artista, genero)");
+                    string valor = PedirString("Ingresa un valor para el criterio");
+                    
+                    if (espotifai.GenerarPlaylist(criterio, valor, nombre))
+                        Console.WriteLine("Lista generada exitosamente.");
+                }
+                
+                else if (comando == "5")
+                {
+                    espotifai.VerMisPlaylists();
+                }
+                
+                else if (comando == "0")
+                {
+                    cerrarPrograma = true;
+                }
+                
+                else
+                {
+                    Console.WriteLine("Comando desconocido.");
+                    ComandosDisponibles();
+                }
+            }
         }
     }
 }
